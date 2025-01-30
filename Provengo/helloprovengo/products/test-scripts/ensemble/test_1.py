ace5aa7c5e38901998fb17e8e1d17d308e414f7b
yairby <104311483+yairby@users.noreply.github.com>
2025-01-30 16:13:26 +0200

    main

---
+++ Provengo/helloprovengo/products/test-scripts/ensemble/test_1.py
@@ -0,0 +1,11 @@
+# Auto-generated python script
+# script number 1
+print("LoginTeacher")
+print("LoginStudent")
+print("ReduceMaxWords")
+print("AnswerQuiz")
+print("   value:	This is a quiz answer with X words.")
+print("DeleteMessage")
+print("ClickMessage")
+
+print("Generated script completed")
---
+++ Provengo/helloprovengo/products/test-scripts/ensemble/test_2.py
@@ -0,0 +1,11 @@
+# Auto-generated python script
+# script number 2
+print("LoginStudent")
+print("AnswerQuiz")
+print("   value:	This is a quiz answer with X words.")
+print("LoginTeacher")
+print("ClickMessage")
+print("ReduceMaxWords")
+print("DeleteMessage")
+
+print("Generated script completed")
